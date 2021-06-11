920b34b13509aa0b098e61c65de103c2cd1d01db
Mulindi <Djangowebapp@gmail.com>
2021-06-11 14:37:15 +0300

    Connects to sqlite database if it does not exist create  one.

--- src/sample/database/DatabaseConnection.java
+++ src/sample/database/DatabaseConnection.java
@@ -1,57 +1,70 @@
 package sample.database;
 
+import java.io.File;
 import java.sql.*;
 
-public class ProductsData {
+public class DatabaseConnection {
 
 
-    public static void connect(){
+    public Connection connect(){
         Connection conn = null;
-//        ResultSet resultSet = null;
         Statement statement = null;
         try{
-           Class.forName("org.sqlite.JDBC");
-            String url = "jdbc:sqlite:Wholesale.db";
-            conn = DriverManager.getConnection(url);
-            statement = conn.createStatement();
-            System.out.println("Connection to sqlite has been established");
-            String sql;
-            int id;
-            sql = "CREATE TABLE \"Product\" (\n" +
-                    "\t\"id\"\tINTEGER NOT NULL,\n" +
-                    "\t\"name\"\tTEXT NOT NULL,\n" +
-                    "\t\"price\"\tINTEGER NOT NULL,\n" +
-                    "\t\"expiryDate\"\tDATE,\n" +
-                    "\tPRIMARY KEY(\"id\" AUTOINCREMENT)\n" +
-                    ");";
-            String sql1 = "Select * FROM Farmers";
-//            sql = "INSERT INTO Products (name, quantity, expiryDate) VALUES ('Beans', '20 bags', 2021-04-23 )";
-            statement.executeUpdate(sql);
-            ResultSet rs= statement.executeQuery(sql1);
-            while (rs.next()){
-                id = rs.getInt("id");
-                System.out.println(id);
+            //Check if the database already exists, if it does connect
+            // to it then set auto commit to false, if not create one with tables
+            File file = new File("Wholesale.db");
+            if (!file.exists()){
+                Class.forName("org.sqlite.JDBC");
+                String url = "jdbc:sqlite:Wholesale.db";
+                conn = DriverManager.getConnection(url);
+                statement = conn.createStatement();
+
+                String sql1 = "CREATE TABLE \"Products\" (\n" +
+                        "\t\"Id\"\tINTEGER NOT NULL,\n" +
+                        "\t\"Name\"\tTEXT NOT NULL,\n" +
+                        "\t\"Amount\"\tINTEGER NOT NULL,\n" +
+                        "\t\"Unit\"\tTEXT NOT NULL,\n" +
+                        "\t\"ExpiryDate\"\tTEXT NOT NULL,\n" +
+                        "\tPRIMARY KEY(\"id\" AUTOINCREMENT)\n" +
+                        ");";
+                String sql2 = "CREATE TABLE \"Farmers\" (\n" +
+                        "\t\"Id\"\tINTEGER NOT NULL,\n" +
+                        "\t\"Products_id\"\tINTEGER NOT NULL,\n" +
+                        "\t\"FirstName\"\tTEXT NOT NULL,\n" +
+                        "\t\"LastName\"\tTEXT NOT NULL,\n" +
+                        "\t\"PhoneNumber\"\tINTEGER NOT NULL,\n" +
+                        "\t\"Price\"\tINTEGER NOT NULL,\n" +
+                        "\tFOREIGN KEY(\"Products_id\") REFERENCES \"Products\",\n" +
+                        "\tPRIMARY KEY(\"Id\" AUTOINCREMENT)\n" +
+                        ");";
+                String sql = "CREATE TABLE \"Retailers\" (\n" +
+                        "\t\"Id\"\tINTEGER NOT NULL,\n" +
+                        "\t\"Products_id\"\tINTEGER NOT NULL,\n" +
+                        "\t\"FirstName\"\tTEXT NOT NULL,\n" +
+                        "\t\"LastName\"\tTEXT NOT NULL,\n" +
+                        "\t\"PhoneNumber\"\tINTEGER NOT NULL,\n" +
+                        "\t\"Price\"\tINTEGER NOT NULL,\n" +
+                        "\tPRIMARY KEY(\"Id\" AUTOINCREMENT),\n" +
+                        "\tFOREIGN KEY(\"Products_id\") REFERENCES \"Products\"\n" +
+                        ");";
+                System.out.println("Created the database");
+                statement.executeUpdate(sql1);
+                statement.executeUpdate(sql2);
+                statement.executeUpdate(sql);
+                statement.close();
+
+            }else{
+                Class.forName("org.sqlite.JDBC");
+                String url = "jdbc:sqlite:Wholesale.db";
+                conn = DriverManager.getConnection(url);
+                System.out.println("Connection to sqlite has been established");
+                conn.setAutoCommit(false);
             }
         }catch (ClassNotFoundException e){
             System.out.println(e.getMessage());
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
-        }finally {
-            try{
-                if(conn != null){
-                    statement.close();
-//                    resultSet.close();
-                    conn.close();
-                }
-            }catch (SQLException ex){
-                System.out.println(ex.getMessage());
-
-            }
         }
+        return conn;
     }
-
-    public static void main(String[] args){
-        connect();
-    }
-
 }
